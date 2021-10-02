2c35d7365499ce544c14ac5f1def0fafb9d34086
Martha <Martha@10.0.0.245>
2021-10-02 10:22:38 -0400

    sunday

--- src/saturday/DemoSaturday.java
+++ src/saturday/DemoSaturday.java
@@ -1,5 +1,8 @@
 package saturday;
 
 public class DemoSaturday {
+	public static void main(String[] args) {
+		System.out.println("Demo Saturday....");
+	}
 
 }
--- src/saturday/DemoSunday.java
+++ src/saturday/DemoSunday.java
@@ -1,5 +1,9 @@
 package saturday;
 
 public class DemoSunday {
+	public static void main(String[] args) {
+		System.out.println("Sunday Demo....");
+		
+	}
 
 }
